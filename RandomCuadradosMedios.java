681239675988a1be3490fab594e9a8efbd4570f6
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-06 20:18:53 -0500

    Renombramos y arreglamos las clases de estos metodos

---
+++ RandomCuadradosMedios.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RandomCuadradosMedios {
+  public List<Double> listaNumeros = new ArrayList<>();
+
+  public RandomCuadradosMedios(int n) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese la semilla: ");
+    int semilla = scanner.nextInt();
+    int numPotencia, numDiv;
+    int aux = semilla;
+    listaNumeros.add(aux / 10000.0);
+    for (int y = 0; y < n; y++) {
+      numPotencia = (int) Math.pow(aux, 2);
+      numDiv = numPotencia / 100;
+      aux = numDiv % 10000;
+      listaNumeros.add(aux / 10000.0);
+    }
+    scanner.close();
+  }
+
+  @Override
+  public String toString() {
+    String numeros = "";
+    for (Double numero : listaNumeros) {
+      numeros += numero + "\n";
+    }
+    return numeros;
+  }
+  // public static void main(String[] args) {
+  // int numInicial = 4808; // Semilla
+  // int n = 10; // Cantidad de números
+  // int[] arrayCuadradosMedios = new int[n];
+
+  // arrayCuadradosMedios = cuadradosMedios(numInicial, n);
+
+  // for (int x = 0; x < n; x++) {
+  // listaNumeros.add(arrayCuadradosMedios[x]);
+  // }
+  // }
+}
---
+++ RandomLehmer.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class RandomLehmer {
+  public List<Double> listaNumeros = new ArrayList<>();
+
+  public RandomLehmer(int n) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Ingrese la semilla: ");
+    int semilla = scanner.nextInt();
+    int aux, numAnterior;
+    numAnterior = semilla;
+    for (int y = 0; y < n; y++) {
+      aux = (48271 * numAnterior) % ((int) Math.pow(2, 31) - 1);
+      listaNumeros.add(aux / Math.pow(2, 31) - 1);
+      numAnterior = aux;
+    }
+    scanner.close();
+  }
+
+  @Override
+  public String toString() {
+    String numeros = "";
+    for (Double numero : listaNumeros) {
+      numeros += numero + "\n";
+    }
+    return numeros;
+  }
+  // public static void main(String[] args) {
+  // int numInicial = 4808; // Semilla
+  // int n = 10; // Cantidad de números
+  // Integer[] arrayLehmer = new Integer[n];
+  // arrayLehmer = lehmer(numInicial, n);
+
+  // for (int x = 0; x < 50; x++) {
+  // listaNumeros.add(arrayLehmer[x]);
+  // }
+  // }
+}
