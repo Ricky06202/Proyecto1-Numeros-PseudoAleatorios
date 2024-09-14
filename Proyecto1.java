21d1852a9ce2064fc0609b8653b392d6ab242309
Ricardo A. Sanjur G <ricardosanjurg@gmail.com>
2024-09-14 12:30:44 -0500

    hice el programa principal

--- Proyecto1.java
+++ Proyecto1.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -5,11 +6,49 @@ import java.util.Scanner;
  */
 public class Proyecto1 {
 
+  static Scanner scanner = new Scanner(System.in);
+
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Ingresa la cantidad de elementos: ");
-    int n = scanner.nextInt();
-    System.out.println(new RandomExcel(n));
+    while (true) {
+      int opcion = menu();
+      System.out.print("Cantidad de Elementos aleatorios: ");
+      int n = scanner.nextInt();
+      List<Double> listaNumeros;
+      switch (opcion) {
+        case 1:
+          listaNumeros = new RandomCuadradosMedios(n).listaNumeros;
+          break;
+        case 2:
+          listaNumeros = new RandomLehmer(n).listaNumeros;
+          break;
+        case 3:
+          listaNumeros = new RandomJava(n).listaNumeros;
+          break;
+        case 4:
+          listaNumeros = new RandomExcel(n).listaNumeros;
+          break;
+      }
+      new PruebaMedias(listaNumeros);
+      new PruebaVarianza(listaNumeros);
+      new PruebaCorrida(listaNumeros);
+    }
     scanner.close();
+  }
+
+  static int menu() {
+    while (true) {
+      System.out.println("1. Random Cuadrados Medios");
+      System.out.println("2. Random Lehmer");
+      System.out.println("3. Random Java");
+      System.out.println("4. Random Excel");
+      System.out.println("5. Salir");
+      System.out.print("Seleccione una opcion: ");
+      int opcion = scanner.nextInt();
+      if (opcion == 5)
+        System.exit(0);
+      if (opcion >= 1 && opcion <= 4)
+        return opcion;
+      System.out.println("Opción no valida");
+    }
   }
 }
